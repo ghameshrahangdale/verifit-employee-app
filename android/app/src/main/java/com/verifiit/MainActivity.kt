80a391bda88d51dd82e804305e653d7ea8ff2e58
Ghamesh Rahangdale <109522027+ghameshrahangdale@users.noreply.github.com>
2026-03-02 16:23:41 +0530

    Initial App setup using boilerplate update logo and theme accordingly

--- android/app/src/main/java/com/verifiit/MainActivity.kt
+++ android/app/src/main/java/com/verifiit/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.rnignite
+package com.verifiit
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/verifiit/MainApplication.kt
+++ android/app/src/main/java/com/verifiit/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.rnignite
+package com.verifiit
 
 import android.app.Application
 import com.facebook.react.PackageList
